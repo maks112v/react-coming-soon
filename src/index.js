ed8789c801203161189a04533dc78d7694f4c51e
Maks <maks112v@gmail.com>
2019-07-06 18:04:01 -0400

    deploy example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Countdown from 'react-countdown-now';
+
 import styles from './index.scss';
 
-export default function ComingSoon({ image, bgColor, bgText, textColor }) {
+export default function ComingSoon({ image, bgColor, textColor, ...rest }) {
+  console.log(rest);
   return (
     <section
       className={styles.fullpage}
@@ -14,7 +17,35 @@ export default function ComingSoon({ image, bgColor, bgText, textColor }) {
       }}
     >
       <div className={styles.content}>
-        <h1>asdfasdf</h1>
+        <div>
+          <h1>Coming Soon</h1>
+          <h3>Stay Tuned</h3>
+          <Countdown
+            date={Date.now() + 1000000000}
+            renderer={props => (
+              <div className={styles.countdown}>
+                {props.days ? (
+                  <div style={{ borderColor: textColor ? textColor : '#000' }}>
+                    <h3>{props.days}</h3>
+                    <p>Days</p>
+                  </div>
+                ) : null}
+                <div style={{ borderColor: textColor ? textColor : '#000' }}>
+                  <h3>{props.hours}</h3>
+                  <p>Hours</p>
+                </div>
+                <div style={{ borderColor: textColor ? textColor : '#000' }}>
+                  <h3>{props.minutes}</h3>
+                  <p>Minutes</p>
+                </div>
+                <div style={{ borderColor: textColor ? textColor : '#000' }}>
+                  <h3>{props.seconds}</h3>
+                  <p>Seconds</p>
+                </div>
+              </div>
+            )}
+          />
+        </div>
       </div>
     </section>
   );
