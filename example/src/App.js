ca8d38c425d39afcb214fb2d7bcf677ed5c2f30c
Maks <maks112v@gmail.com>
2019-07-06 20:38:05 -0400

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 
 //import ComingSoon from 'react-coming-soon';
 
-import ComingSoon from 'react-coming-soon';
-
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ComingSoon
+        {/* <ComingSoon
           //image="https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2110&q=80"
           bgColor="#fff"
           textColor="#000"
           illustration="development"
-        />
+        /> */}
       </div>
     );
   }
