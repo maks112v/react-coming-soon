c42f358565beea07f37e60691e9521462665014c
Maks <maks112v@gmail.com>
2019-07-09 22:28:13 -0400

    added sticky

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ComingSoon from 'react-coming-soon';
 import { TwitterPicker } from 'react-color';
 import DateTimePicker from 'react-datetime-picker';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 import './index.css';
 
@@ -139,229 +140,253 @@ export default function App() {
             Generate A Coming Soon Page
           </h2>
 
-          <MDBRow>
-            <MDBCol md="6" style={{ textAlign: 'center' }}>
-              <MDBInput
-                label="Title"
-                value={demoComingSoon.title}
-                onChange={e =>
-                  setDemoComingSoon({
-                    ...demoComingSoon,
-                    title: e.target.value,
-                  })
-                }
-              />
-              <MDBInput
-                label="Subtitle"
-                value={demoComingSoon.subtitle}
-                onChange={e =>
-                  setDemoComingSoon({
-                    ...demoComingSoon,
-                    subtitle: e.target.value,
-                  })
-                }
-              />
-              <h6>Text Color</h6>
-              <TwitterPicker
-                width="auto"
-                colors={[
-                  '#212121',
-                  '#FAFAFA',
-                  '#3F51B5',
-                  '#FF6900',
-                  '#FCB900',
-                  '#7BDCB5',
-                  '#00D084',
-                  '#8ED1FC',
-                  '#03A9F4',
-                  '#0693E3',
-                  '#ABB8C3',
-                  '#EB144C',
-                  '#f44336',
-                  '#F78DA7',
-                  '#9900EF',
-                ]}
-                onChange={e =>
-                  setDemoComingSoon({ ...demoComingSoon, textColor: e.hex })
-                }
-              />
-              <br />
-              <h6>Background Color</h6>
-              <TwitterPicker
-                width="auto"
-                colors={[
-                  '#212121',
-                  '#FAFAFA',
-                  '#3F51B5',
-                  '#FF6900',
-                  '#FCB900',
-                  '#7BDCB5',
-                  '#00D084',
-                  '#8ED1FC',
-                  '#03A9F4',
-                  '#0693E3',
-                  '#ABB8C3',
-                  '#EB144C',
-                  '#f44336',
-                  '#F78DA7',
-                  '#9900EF',
-                ]}
-                onChange={e =>
-                  setDemoComingSoon({ ...demoComingSoon, bgColor: e.hex })
-                }
-              />
-              <h6 className="mt-4">Finish Date</h6>
-              <DateTimePicker
-                value={demoComingSoon.date}
-                onChange={e =>
-                  setDemoComingSoon({ ...demoComingSoon, date: e })
-                }
-              />
-              <MDBInput
-                label="Image"
-                value={demoComingSoon.image}
-                onChange={e =>
-                  setDemoComingSoon({
-                    ...demoComingSoon,
-                    image: e.target.value,
-                  })
-                }
-              />
-              <h6 className="mt-4">Select an Illustration</h6>
-              <MDBRow>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({ ...demoComingSoon, illustration: '' })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/404.svg"
-                      style={{ width: '100%' }}
-                      alt="404 SVG"
-                    />
-                    <p>None</p>
-                  </div>
-                </MDBCol>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({
-                        ...demoComingSoon,
-                        illustration: 'development',
-                      })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/under-development.png"
-                      style={{ width: '100%' }}
-                      alt="Under Development"
-                    />
-                    <p>Under Development</p>
-                  </div>
-                </MDBCol>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({
-                        ...demoComingSoon,
-                        illustration: 'react',
-                      })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/react.svg"
-                      style={{ width: '100%' }}
-                      alt="React SVG"
-                    />
-                    <p>React</p>
-                  </div>
-                </MDBCol>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({
-                        ...demoComingSoon,
-                        illustration: 'git',
-                      })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/git.svg"
-                      style={{ width: '100%' }}
-                      alt="Github"
-                    />
-                    <p>Github</p>
-                  </div>
-                </MDBCol>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({
-                        ...demoComingSoon,
-                        illustration: 'planning',
-                      })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/planning.svg"
-                      style={{ width: '100%' }}
-                      alt="Planning SVG"
-                    />
-                    <p>Planning</p>
-                  </div>
-                </MDBCol>
-                <MDBCol sm="6">
-                  <div
-                    onClick={() =>
-                      setDemoComingSoon({
-                        ...demoComingSoon,
-                        illustration: 'web-development',
-                      })
-                    }
-                  >
-                    <img
-                      src="https://react-coming-soon.maksv.me/web-development.svg"
-                      style={{ width: '100%' }}
-                      alt="Web Development SVG"
-                    />
-                    <p>Web Development</p>
-                  </div>
-                </MDBCol>
-              </MDBRow>
+          <StickyContainer>
+            <MDBRow>
+              <MDBCol md="6" style={{ textAlign: 'center' }}>
+                <MDBInput
+                  label="Title"
+                  value={demoComingSoon.title}
+                  onChange={e =>
+                    setDemoComingSoon({
+                      ...demoComingSoon,
+                      title: e.target.value,
+                    })
+                  }
+                />
+                <MDBInput
+                  label="Subtitle"
+                  value={demoComingSoon.subtitle}
+                  onChange={e =>
+                    setDemoComingSoon({
+                      ...demoComingSoon,
+                      subtitle: e.target.value,
+                    })
+                  }
+                />
+                <h6>Text Color</h6>
+                <TwitterPicker
+                  width="auto"
+                  colors={[
+                    '#212121',
+                    '#FAFAFA',
+                    '#3F51B5',
+                    '#FF6900',
+                    '#FCB900',
+                    '#7BDCB5',
+                    '#00D084',
+                    '#8ED1FC',
+                    '#03A9F4',
+                    '#0693E3',
+                    '#ABB8C3',
+                    '#EB144C',
+                    '#f44336',
+                    '#F78DA7',
+                    '#9900EF',
+                  ]}
+                  onChange={e =>
+                    setDemoComingSoon({ ...demoComingSoon, textColor: e.hex })
+                  }
+                />
+                <br />
+                <h6>Background Color</h6>
+                <TwitterPicker
+                  width="auto"
+                  colors={[
+                    '#212121',
+                    '#FAFAFA',
+                    '#3F51B5',
+                    '#FF6900',
+                    '#FCB900',
+                    '#7BDCB5',
+                    '#00D084',
+                    '#8ED1FC',
+                    '#03A9F4',
+                    '#0693E3',
+                    '#ABB8C3',
+                    '#EB144C',
+                    '#f44336',
+                    '#F78DA7',
+                    '#9900EF',
+                  ]}
+                  onChange={e =>
+                    setDemoComingSoon({ ...demoComingSoon, bgColor: e.hex })
+                  }
+                />
+                <h6 className="mt-4">Finish Date</h6>
+                <DateTimePicker
+                  value={demoComingSoon.date}
+                  onChange={e =>
+                    setDemoComingSoon({ ...demoComingSoon, date: e })
+                  }
+                />
+                <MDBInput
+                  label="Image"
+                  value={demoComingSoon.image}
+                  onChange={e =>
+                    setDemoComingSoon({
+                      ...demoComingSoon,
+                      image: e.target.value,
+                    })
+                  }
+                />
+                <h6 className="mt-4">Select an Illustration</h6>
 
-              <MDBBtn
-                color="info"
-                onClick={() => setstate({ ...state, generateDemo: true })}
-              >
-                Live Demo
-              </MDBBtn>
-            </MDBCol>
-            <MDBCol md="6">
-              <h5>Copy this code to get started</h5>
-              <div
-                style={{
-                  backgroundColor: 'black',
-                  borderRadius: 5,
-                  padding: '10px 7px',
-                }}
-              >
-                <code lang="javascript" style={{ color: 'white' }}>
-                  {`<ComingSoon
-  ${demoComingSoon.title && `title="${demoComingSoon.title}"`}
-  ${demoComingSoon.subtitle && `subtitle="${demoComingSoon.subtitle}"`}
-  ${demoComingSoon.image && `image="${demoComingSoon.image}"`}
-  ${demoComingSoon.bgColor && `bgColor="${demoComingSoon.bgColor}"`}
-  ${demoComingSoon.textColor && `textColor="${demoComingSoon.textColor}"`}
-  ${demoComingSoon.date && `date="${demoComingSoon.date}"`}
-  ${demoComingSoon.illustration &&
-    `illustration="${demoComingSoon.illustration}"`}
-  />
-`}
-                </code>
-              </div>
-            </MDBCol>
-          </MDBRow>
+                <MDBRow>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: '',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/404.svg"
+                        style={{ width: '100%' }}
+                        alt="404 SVG"
+                      />
+                      <p>None</p>
+                    </div>
+                  </MDBCol>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: 'development',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/under-development.png"
+                        style={{ width: '100%' }}
+                        alt="Under Development"
+                      />
+                      <p>Under Development</p>
+                    </div>
+                  </MDBCol>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: 'react',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/react.svg"
+                        style={{ width: '100%' }}
+                        alt="React SVG"
+                      />
+                      <p>React</p>
+                    </div>
+                  </MDBCol>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: 'git',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/git.svg"
+                        style={{ width: '100%' }}
+                        alt="Github"
+                      />
+                      <p>Github</p>
+                    </div>
+                  </MDBCol>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: 'planning',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/planning.svg"
+                        style={{ width: '100%' }}
+                        alt="Planning SVG"
+                      />
+                      <p>Planning</p>
+                    </div>
+                  </MDBCol>
+                  <MDBCol sm="6">
+                    <div
+                      onClick={() =>
+                        setDemoComingSoon({
+                          ...demoComingSoon,
+                          illustration: 'web-development',
+                        })
+                      }
+                    >
+                      <img
+                        src="https://react-coming-soon.maksv.me/web-development.svg"
+                        style={{ width: '100%' }}
+                        alt="Web Development SVG"
+                      />
+                      <p>Web Development</p>
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCol>
+              <MDBCol md="6">
+                <Sticky>
+                  {({ style }) => (
+                    <div
+                      style={{ ...style, marginTop: 50 }}
+                      className="mobileSticky"
+                    >
+                      <h5>First install our package</h5>
+                      <code>npm i react-coming-soon</code>
+                      <h5 style={{ marginTop: 20 }}>
+                        Then import the package into your App.js
+                      </h5>
+                      <code>import ComingSoon from 'react-coming-soon'</code>
+                      <h5 style={{ marginTop: 20 }}>
+                        Customize and copy the code below into your project.
+                      </h5>
+                      <MDBBtn
+                        color="info"
+                        onClick={() =>
+                          setstate({ ...state, generateDemo: true })
+                        }
+                      >
+                        Live Preview
+                      </MDBBtn>
+                      <div
+                        style={{
+                          backgroundColor: 'black',
+                          borderRadius: 5,
+                          padding: '10px 7px',
+                        }}
+                      >
+                        <code lang="javascript" style={{ color: 'white' }}>
+                          {`<ComingSoon
+    ${demoComingSoon.title && `title="${demoComingSoon.title}"`}
+    ${demoComingSoon.subtitle && `subtitle="${demoComingSoon.subtitle}"`}
+    ${demoComingSoon.image && `image="${demoComingSoon.image}"`}
+    ${demoComingSoon.bgColor && `bgColor="${demoComingSoon.bgColor}"`}
+    ${demoComingSoon.textColor && `textColor="${demoComingSoon.textColor}"`}
+    ${demoComingSoon.date && `date="${demoComingSoon.date}"`}
+    ${demoComingSoon.illustration &&
+      `illustration="${demoComingSoon.illustration}"`}
+    />
+  `}
+                        </code>
+                      </div>
+                    </div>
+                  )}
+                </Sticky>
+              </MDBCol>
+            </MDBRow>
+          </StickyContainer>
         </section>
       </MDBContainer>
       <MDBFooter className="font-small mt-4">
